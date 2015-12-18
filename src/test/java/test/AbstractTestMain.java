e3519dfd6f784bb494aaa2f7ac2ada0db66351ed
pollseed <cb.so.devope+0001@gmail.com>
2015-12-18 17:27:22 +0900

    Update AbstractTestMain.java

--- src/test/java/test/AbstractTestMain.java
+++ src/test/java/test/AbstractTestMain.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
 
-public class AbstractTestMain {
+import org.apache.commons.lang.StringUtils;
 
+public class AbstractTestMain {
     protected void eq(final String expected, final String actual) {
+        t$0(expected, actual);
         assertEquals(expected, actual);
     }
 
@@ -15,10 +17,25 @@ public class AbstractTestMain {
     }
 
     protected void ng(final String expected, final String actual) {
+        t$0(expected, actual);
         assertNotSame(expected, actual);
     }
 
     protected void ng(final String expected, final boolean actual) {
+        t$0(expected);
         assertFalse(expected, actual);
+    }
+
+    private void t$0(final String expected, final String actual) {
+        a$0(StringUtils.isBlank(expected) || StringUtils.isBlank(actual));
+    }
+
+    private void t$0(final String expected) {
+        a$0(StringUtils.isBlank(expected));
+    }
+
+    private void a$0(boolean a) {
+        if (a)
+            throw new AssertionError("args bug error");
     }
 }
