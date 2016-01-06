bb312842f9a4968d37668163df11bf8462166462
pollseed <cb.so.devope+0001@gmail.com>
2016-01-06 17:12:45 +0900

    Update PreparedStatementTest.java

--- src/test/java/test/PreparedStatementTest.java
+++ src/test/java/test/PreparedStatementTest.java
@@ -47,3 +47,27 @@ public class PreparedStatementTest {
         assertEquals(executeUpdate3, 0);
     }
 }
+
+
+/*
+メモ
+
+DDL
+
+CREATE SCHEMA `new_schema` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `employees` (
+   `id` int(11) NOT NULL,
+   `fname` varchar(30) DEFAULT NULL,
+   `lname` varchar(30) DEFAULT NULL,
+   `hired` date NOT NULL DEFAULT '1970-01-01',
+   `separated` date NOT NULL DEFAULT '9999-12-31',
+   `job_code` int(11) NOT NULL,
+   `store_id` int(11) NOT NULL,
+   PRIMARY KEY (`id`,`job_code`,`store_id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+ /*!50100 PARTITION BY RANGE (store_id)
+ (PARTITION p0 VALUES LESS THAN (6) ENGINE = InnoDB,
+  PARTITION p1 VALUES LESS THAN (11) ENGINE = InnoDB,
+  PARTITION p2 VALUES LESS THAN (16) ENGINE = InnoDB,
+  PARTITION p3 VALUES LESS THAN (21) ENGINE = InnoDB) */
